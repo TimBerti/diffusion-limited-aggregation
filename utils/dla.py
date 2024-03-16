3741b0c0a1f2588aca10c0ece407e64ddf22676f
Tim Berti <tim.fabian.berti@gmail.com>
2024-03-16 22:37:39 +0100

    probability distribution

--- utils/dla.py
+++ utils/dla.py
@@ -3,7 +3,7 @@ import numpy as np
 def get_neighbors(grid):
     return np.roll(grid, 1, axis=0) + np.roll(grid, -1, axis=0) + np.roll(grid, 1, axis=1) + np.roll(grid, -1, axis=1)
 
-def new_position(grid):
+def new_start(grid):
     n = grid.shape[0]
     x, y = n//2, n//2
     while grid[x, y] == 1:
@@ -12,15 +12,24 @@ def new_position(grid):
         y = int(n//2 * np.sin(theta) + n//2)
     return np.array([x, y])
 
-def step(grid, current, neighbors):
-    n = grid.shape[0]
+def step(current, n):
     directions = np.array([[1, 0], [0, 1], [-1, 0], [0, -1]])
     direction = directions[np.random.randint(0, 4)]
     new = current + direction
     new = np.mod(new, n)
-    if neighbors[*new] > 0:
-        return new, True
-    return new, False
+    return new
+
+def next_aggregation(current, neighbors):
+    while neighbors[*current] == 0:
+        current = step(current, neighbors.shape[0])
+    return current
+
+def probability_distribution(grid, samples=4):
+    distribution = np.zeros(grid.shape)
+    for _ in range(samples):
+        aggregation = next_aggregation(new_start(grid), get_neighbors(grid))
+        distribution[*aggregation] += 1
+    return distribution / samples
 
 def radius_of_gyration(grid):
     x, y = np.where(grid == 1)
